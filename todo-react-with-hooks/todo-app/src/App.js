e482847fe1fe29e346fde7cd1e1f9fa76c41537b
Alberto Garcia <mmhgarcia@gmail.com>
2021-03-09 12:33:13 -0400

    Se crea la clase de css Titulo. Se aplica estilo al titulo de la UI y se añade un input text para cargar los Todos.

--- todo-react-with-hooks/todo-app/src/App.js
+++ todo-react-with-hooks/todo-app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+
+//import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let classTitulo = 'Titulo';
+
+    const [contador, setContador] = useState(0);
+    
+    return (
+    
+        <div className="App">
+        
+            <header className="App-header">
+                        
+                <label className="Titulo">todos</label>
+
+                <div>
+                    <input placeholder="Enter a value" />
+                    <button>Add</button>
+                </div>
+        
+            </header>
+        
+        </div>
+    );
 }
 
 export default App;
