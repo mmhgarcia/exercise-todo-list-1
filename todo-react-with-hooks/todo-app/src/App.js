37720de9559ec2524b0124a4741cd7169d3bf8a1
Alberto Garcia <mmhgarcia@gmail.com>
2021-03-09 20:29:08 -0400

    Se añade bootstrap y react-bootstrap. Se añaden clases css para formato. Se maneja el evento enter para añadir todos, cada nuevo todo es añadido a la lista, se permite la eliminación de cualquier todo, al pie de la lista hay un contador que indica la cantidad de todos que hay en la lista.

--- todo-react-with-hooks/todo-app/src/App.js
+++ todo-react-with-hooks/todo-app/src/App.js
@@ -1,31 +1,111 @@
-import React, { useState } from 'react'
+import React from "react";
+import "./App.css";
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-//import logo from './logo.svg';
+function CountTodos( { todos } ) {
+    return (
+        <div>
+            <span>{todos.length} items left.</span>            
+        </div>
+    );
+}
 
-import './App.css';
+function Todo({ todo, index, removeTodo }) {
+    return (
+        <div  className="todo">
+            <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
+            <div>             
+                <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>
+            </div>
+        </div>
+    );
+}
+
+function FormTodo({ addTodo }) {
+    const [value, setValue] = React.useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo(value);
+        setValue("");
+    };
+
+    return (
+        <Form onSubmit={handleSubmit}>
+        
+            <Form.Group>                
+                <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
+            </Form.Group>            
+        
+        </Form>
+    );
+}
 
 function App() {
+    const [todos, setTodos] = React.useState([
+        {
+            text: "This is a sampe todo",
+            isDone: false
+        }
+    ]);
 
-    let classTitulo = 'Titulo';
+    const addTodo = text => {
+        const newTodos = [...todos, { text }];
+        setTodos(newTodos);
+    };
 
-    const [contador, setContador] = useState(0);
-    
+    const markTodo = index => {
+        const newTodos = [...todos];
+        newTodos[index].isDone = true;
+        setTodos(newTodos);
+    };
+
+    const removeTodo = index => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+
     return (
     
-        <div className="App">
-        
-            <header className="App-header">
+        <div className="app" style={{ paddingTop: "40px" }}>
+            
+            <div className="container" class="bg-light" style={{ paddingTop: "40px", paddingBottom: "40px" }}>
+                
+                <div style={{ backgroundColor: "white", marginLeft: "5%", marginRight: "5%" } }>
+                
+                    <h1 id="titulo" class="display-1 text-center">todos</h1>
+                
+                    <FormTodo addTodo={addTodo} />
+                    
+                    <div>
+                
+                        {todos.map((todo, index) => (
+                            <Card>
+                                <Card.Body>
+                                    <Todo
+                                        key={index}
+                                        index={index}
+                                        todo={todo}                                   
+                                        removeTodo={removeTodo}
+                                    />
+                                </Card.Body>
+                            </Card>
+                        ))}
                         
-                <label className="Titulo">todos</label>
-
-                <div>
-                    <input placeholder="Enter a value" />
-                    <button>Add</button>
+                        <br />
+                        <CountTodos todos={todos} />
+                
+                    </div>
+                
                 </div>
         
-            </header>
-        
+            </div>
+
         </div>
+    
     );
 }
 
