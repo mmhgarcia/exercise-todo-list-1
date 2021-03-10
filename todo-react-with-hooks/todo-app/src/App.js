ab96e05b350362171bcf6c264dc99aa58c6cc4ce
Alberto Garcia <mmhgarcia@gmail.com>
2021-03-09 23:03:45 -0400

    Se hace revision final, se comenta el codigo y se hacen ajustes en las fuentes. Se procede con el PUSH.

--- todo-react-with-hooks/todo-app/src/App.js
+++ todo-react-with-hooks/todo-app/src/App.js
@@ -3,18 +3,28 @@ import "./App.css";
 import { Button, Card, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+/* Muestra , al pie de la lista, la cantidad de 'todos' que contiene */
 function CountTodos( { todos } ) {
     return (
         <div>
-            <span style={{ fontWeight: 'bold' , color: 'gray'}}>{todos.length} items left.</span>
+            <span style={{ fontWeight: 'bold', color: 'gray' }}>
+                {todos.length} items en lista.
+            </span>
         </div>
     );
 }
 
+/* 
+    Define la presentacion de UN TODO ( texto y el boton de eliminar)
+    Recibe 3 parametros: 
+        'todo
+        indice
+        la funcion encargada de eliminar
+*/
 function Todo({ todo, index, removeTodo }) {
     return (
-        <div  className="todo">
-            <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
+        <div  class="todo">
+            <span  style={{fontSize: "1.2em"}}>{todo.text}</span>
             <div>             
                 <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>
             </div>
@@ -22,9 +32,16 @@ function Todo({ todo, index, removeTodo }) {
     );
 }
 
+/*  
+    Este es el formulario que presenta el titulo y la caja de texto.
+    Solo si se ingresa un valor, este es añadido a la lista.
+*/
 function FormTodo({ addTodo }) {
+
+    /*Hook para manejar el valor del texto ingresado */
     const [value, setValue] = React.useState("");
 
+    /* Al pulsar enter en la caja de texto se llama a este evento  */
     const handleSubmit = e => {
         e.preventDefault();
         if (!value) return;
@@ -35,8 +52,17 @@ function FormTodo({ addTodo }) {
     return (
         <Form onSubmit={handleSubmit}>
         
+            {/* Titulo de la UI */}
+            <h1 id="titulo" class="display-1 text-center">todos</h1>
+
+            {/* Caja de texto para ingresar el 'todo' */}
             <Form.Group>                
-                <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
+                <Form.Control type="text"
+                    className="input"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                    style={{ fontSize: "1.4em" }}
+                    placeholder="Ingrese nuevo TODO y presione Enter." />
             </Form.Group>            
         
         </Form>
@@ -44,24 +70,33 @@ function FormTodo({ addTodo }) {
 }
 
 function App() {
+
+    /* Hook  para controlar la lista de 'todos' */
     const [todos, setTodos] = React.useState([
         {
-            text: "This is a sampe todo",
+            text: "Este es un todo de prueba cargado al iniciar.",
+            isDone: false
+        },
+        {
+            text: "Ir al cine.",
+            isDone: false
+        },
+        {
+            text: "Ir a la playa.",
             isDone: false
         }
     ]);
 
+    /* Añade un 'todo' a la lista */
     const addTodo = text => {
         const newTodos = [...todos, { text }];
         setTodos(newTodos);
     };
 
-    const markTodo = index => {
-        const newTodos = [...todos];
-        newTodos[index].isDone = true;
-        setTodos(newTodos);
-    };
-
+    /*
+        Elimina un 'todo' de la lista 
+        recibe como parametro el indice del item a eliminar    
+    */
     const removeTodo = index => {
         const newTodos = [...todos];
         newTodos.splice(index, 1);
@@ -72,16 +107,16 @@ function App() {
     
         <div className="app" style={{ paddingTop: "40px" }}>
             
-            <div className="container" class="bg-light" style={{ paddingTop: "40px", paddingBottom: "40px", height: "95%" , overflow: "scroll" }}>
+            <div class="container bg-light" style={{ paddingTop: "40px", paddingBottom: "40px", height: "95%", overflow: "scroll" }}>
                 
-                <div style={{ backgroundColor: "white", marginLeft: "5%", marginRight: "5%" } }>
-                
-                    <h1 id="titulo" class="display-1 text-center">todos</h1>
-                
-                    <FormTodo addTodo={addTodo} />
+                <div style={{ backgroundColor: "white", marginLeft: "5%", marginRight: "5%", paddingLeft: "25px", paddingRight: "25px" } }>
+
+                    { /* Formulario para ingreso de texto */}
+                    <FormTodo addTodo={addTodo}  />
                     
                     <div>
                 
+                        { /* Presenta el contenido del array 'todos' en forma de lista */}
                         {todos.map((todo, index) => (
                             <Card>
                                 <Card.Body>
@@ -96,6 +131,8 @@ function App() {
                         ))}
                         
                         <br />
+
+                        { /* Presenta al pie de lista el numero de elementos de la lista. */}
                         <CountTodos todos={todos} />
                 
                     </div>
